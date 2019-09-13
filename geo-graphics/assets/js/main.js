749be079f104d36224091b74476f11b6e27c13fa
Amelia Clark <amelia17@live.com.au>
2019-09-13 14:08:20 +1000

    new geo graphics

---
+++ geo-graphics/assets/js/main.js
@@ -0,0 +1,12 @@
+setTimeout(function() { // start a delay
+  var fade = document.getElementById("fade"); // get required element
+  fade.style.opacity = 1; // set opacity for the element to 1
+  var timerId = setInterval(function() { // start interval loop
+    var opacity = fade.style.opacity; // get current opacity
+    if (opacity == 0) { // check if its 0 yet
+      clearInterval(timerId); // if so, exit from interval loop
+    } else {
+      fade.style.opacity = opacity - 0.05; // else remove 0.05 from opacity
+    }
+  }, 100); // run every 0.1 second
+}, 3000); // wait to run after 3 seconds
