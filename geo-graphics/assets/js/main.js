882331c4b0fa6a6795a4b6f2aa8541b4e0925338
Amelia Clark <amelia17@live.com.au>
2019-10-11 14:13:29 +1100

    new files

--- geo-graphics/assets/js/main.js
+++ geo-graphics/assets/js/main.js
@@ -1,12 +1,12 @@
-setTimeout(function() { // start a delay
-  var fade = document.getElementById("fade"); // get required element
-  fade.style.opacity = 1; // set opacity for the element to 1
-  var timerId = setInterval(function() { // start interval loop
-    var opacity = fade.style.opacity; // get current opacity
-    if (opacity == 0) { // check if its 0 yet
-      clearInterval(timerId); // if so, exit from interval loop
+setTimeout(function() { // 
+  var fade = document.getElementById("fade"); // 
+  fade.style.opacity = 1; // 
+  var timerId = setInterval(function() { // 
+    var opacity = fade.style.opacity; // 
+    if (opacity == 0) { // 
+      clearInterval(timerId); // 
     } else {
-      fade.style.opacity = opacity - 0.05; // else remove 0.05 from opacity
+      fade.style.opacity = opacity - 0.05; // 
     }
-  }, 100); // run every 0.1 second
-}, 3000); // wait to run after 3 seconds
+  }, 100); // 
+}, 3000); // 
